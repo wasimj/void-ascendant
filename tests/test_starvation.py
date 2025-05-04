738fde9232cbf6b4e7903b2ba29055fefb856839
Wasim <w@juned.net>
2025-05-04 08:38:12 +0100

    make headless

--- tests/test_starvation.py
+++ tests/test_starvation.py
@@ -25,6 +25,9 @@ class StarvationTest(unittest.TestCase):
                 if browser == 'chrome':
                     from selenium.webdriver.chrome.options import Options
                     options = Options()
+                    options.add_argument("--headless")
+                    options.add_argument("--window-size=1366,768")
+                    options.add_argument("--disable-gpu")
                     options.add_argument("--no-sandbox")
                     options.add_argument("--disable-dev-shm-usage")
                     self.driver = webdriver.Chrome(options=options)
